c734cb22eaa919f88e2d57ad7d1766e84a561bf6
umairdalvi <dev.ud02@proton.me>
2025-10-20 17:04:33 +0530

    create HeaderLogo component

---
+++ src/components/HeaderLogo.jsx
@@ -0,0 +1,10 @@
+const HeaderLogo = () => {
+    return (
+        <a href="#" className="header-logo-wrapper flex align-center">
+            <img className="header-logo" src="/images/around-logo.svg" alt="logo" />
+            <span className="header-logo-text">Around</span>
+        </a>
+    )
+}
+
+export default HeaderLogo

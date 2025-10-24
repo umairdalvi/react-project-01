523c3be9df15b6ce862b9092e4e043dc934cf82e
umairdalvi <dev.ud02@proton.me>
2025-10-24 12:30:53 +0530

    create Clients section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header"
 import Hero from "./components/Hero"
 import "../src/App.css"
+import Clients from "./components/Clients"
 
 const App = () => {
   return (
     <div>
-      <Header></Header>
+      <Header />
       <Hero />
+      <Clients />
     </div>
   )
 }
---
+++ src/components/Clients.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Clients = () => {
+
+  return (
+    <div className='container'>
+      <section className="clients">
+        <div className="clients-content flex justify-between">
+          <img src="./images/clutch-rating-dark.svg" alt="rating" />
+          <img src="./images/deloitte-logo.svg" alt="deloitte" />
+          <img src="./images/vuejs-logo.svg" alt="vuejs" />
+          <img src="./images/awwwards-logo.svg" alt="awwwads" />
+          <img src="./images/clutch-logo.svg" alt="clutch-logo" />
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Clients

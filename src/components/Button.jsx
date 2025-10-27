52dc0a8a5e8d008306aa0539da7ac388bdc5cac3
umairdalvi <dev.ud02@proton.me>
2025-10-27 16:16:18 +0530

    update Button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,12 +2,13 @@ const Button = ({
   text,
   color = 'primary',
   fillType = 'solid',
+  shape = null,
   icon = null,
   style = null,
   iconPosition = 'left'
 }) => {
 
-  const buttonClasses = `btn btn-${color} btn-${fillType} ${style ? `btn-${style}` : ""}`;
+  const buttonClasses = `btn btn-${color} btn-${fillType} ${style ? `btn-${style}` : ""} ${shape ? `btn-${shape}` : ""}`;
 
   return (
     <button className={buttonClasses}>
